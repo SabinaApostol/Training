e3fcf7c6a1aa09989aff80e375d58f367d32ac73
SabinaApostol <sabina.apostol@5psolutions.eu>
2021-11-26 13:01:54 +0200

    #25 order.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-sabina-apostol/tasks/25 add content in order.php

--- order.php
+++ order.php
@@ -1,4 +1,92 @@
 <?php
 
-echo 'Id-ul comenzii este: ';
-echo $_POST['idOrder'];
+require_once 'common.php';
+
+if (! empty($_POST['idOrder'])) {
+
+    $stmt = $conn->prepare('SELECT * FROM orders WHERE id = ' . $_POST['idOrder']);
+    $stmt->execute();
+    $order = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    $products = unserialize($order['purchasedProducts']);
+    $customerDetails = unserialize($order['customerDetails']);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= translate('Document') ?></title>
+    <style>
+        h1 {
+            text-align: center;
+            font-size: 50pt;
+        }
+        table, th, td {
+            border: 1px solid #000000;
+            text-align: center;
+        }
+        .center{
+            margin-left: auto;
+            margin-right: auto;
+        }
+        img {
+            height: 30px;
+            width: 30px;
+        }
+        p, ul {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1><?= translate('Order') ?></h1>
+    <p class="center"><?= translate('Checkout information: ') ?></p>
+    <table class="center">
+        <tr>
+            <th><?= translate('Date') ?></th>
+            <th><?= translate('Name') ?></th>
+            <th><?= translate('Email') ?></th>
+        </tr>
+        <tr>
+            <td>
+                <?= $order['creationDate'] ?>
+            </td>
+            <td>
+                <?= $customerDetails['name'] ?>
+            </td>
+            <td>
+                <?= $customerDetails['email'] ?>
+            </td>
+        </tr>
+    </table>
+    <br>
+    <p class="center"><?= translate('Products: ') ?></p>
+    <table class="center">
+        <tr>
+            <th><?= translate('Image') ?></th>
+            <th><?= translate('Title') ?></th>
+            <th><?= translate('Description') ?></th>
+            <th><?= translate('Price') ?></th>
+        </tr>
+        <?php foreach ($products as $product) : ?>
+            <tr> 
+                <td>
+                    <img src="<?= $product->img ?>">
+                </td>
+                <td>
+                    <?= $product->title ?>
+                </td>
+                <td>
+                    <?= $product->description ?>
+                </td>
+                <td>
+                    <?= $product->price ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
